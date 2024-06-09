6fbf257408d87bbba0560eb7417e0d7a2bfad341
salmanti <151623707+salmanti@users.noreply.github.com>
2024-06-09 13:17:56 +0600

    first commit

---
+++ 09June2024/php_syntax.php
@@ -0,0 +1,15 @@
+<?php
+// full syntax
+echo "Hello World <br>";
+
+?>
+
+<?
+// short open Tag
+echo "Hello World with short tag";
+?>
+
+<?= "Hello World with short circuit<br>"
+// short circuit
+
+?>
