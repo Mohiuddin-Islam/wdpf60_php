0dab29d7c6f910d285265d825a3bb2ca05dd06a4
MOHIUDDIN ISLAM <mohiuddincr7@gmail.com>
2024-06-09 21:49:56 +0600

    09 june 2024

--- 09June2024/php_syntax.php
+++ 09June2024/php_syntax.php
@@ -1,15 +1,22 @@
 <?php
 // full syntax
 echo "Hello World <br>";
+echo "Good Morning<br>"
+
 
 ?>
 
-<?
+<?php
 // short open Tag
-echo "Hello World with short tag";
+echo "Hello World with short tag <br>";
+
+echo "I love my Country<br>"
+
+
 ?>
 
 <?= "Hello World with short circuit<br>"
 // short circuit
+
 
 ?>
