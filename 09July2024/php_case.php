e89257ded983d14beea7d55eaee670e452cd307c
salmanti <151623707+salmanti@users.noreply.github.com>
2024-07-09 12:49:02 +0600

    09/07/2024

---
+++ 09July2024/php_case.php
@@ -0,0 +1,16 @@
+<?php
+
+$str = "today is saturday";
+echo $upper = strtoupper($str);
+
+echo "<hr>";
+echo strtolower($str);
+echo "<hr>";
+echo ucfirst($str);
+echo "<hr>";
+echo ucwords($str);
+echo "<hr>";
+echo lcfirst($upper);
+
+
+?>
